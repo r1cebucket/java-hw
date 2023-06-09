000b3cbdd05d2ea648e0f6bc384ac5760956ea8d
FanHang <ricebucket.mail@gmail.com>
2023-06-09 17:16:08 +0800

    update

--- Assignment2/PartIII/EstimateLog.java
+++ Assignment2/PartIII/EstimateLog.java
@@ -1,7 +1,5 @@
-
 public class EstimateLog {
-
-	public static double estimateLog(double x) { // TODO
+	public static double estimateLog(double x) { //
 		double result = 0.;
 		int n;
 		for (n = 1;; n++) {
@@ -11,19 +9,17 @@ public class EstimateLog {
 				break;
 			}
 		}
-		System.out.println(String.format("ln(%.1f) is ", x + 1.) + result);
+		System.out.println(String.format("ln(%.2f) is ", x + 1.) + result);
 		System.out.print(String.format(
-				"ln(%.1f) is %f\nit requires %d iterations to estimate ln(%.1f) within .0001\n\n",
-				x + 1., result, n, x + 1.));
+				"it requires %d iterations to estimate ln(%.2f) within .0001\n\n",
+				n, x + 1.));
 		return result;
-
 	}
 
 	public static void main(String[] args) {
-		for (double x = 0; x < 1.; x += .01) {
+		for (double x = 0.; x < .91; x += .01) {
 			EstimateLog.estimateLog(x);
 		}
 
 	}
-
 }
--- Assignment2/PartIV/Permutations.java
+++ Assignment2/PartIV/Permutations.java
@@ -1,11 +1,12 @@
-
 class Permutations {
-
 	public static int[][] getPermutations(int[] array) {
+		int size = 1;
+		for (int i = array.length; i > 0; i--) {
+			size *= i;
+		}
+		int[][] permutations = new int[size][0]; // obviously your array will have more members
 
-		int[][] permutations = new int[0][array.length]; // obviously your array will have more members
 		return permutations;
-
 	}
 
 	private static int currentIndex = 0;
@@ -13,6 +14,6 @@ class Permutations {
 	public static void main(String[] args) {
 		int[] startingArray = { 1, 2, 3, 4, 5 };
 		int[][] result = getPermutations(startingArray);
-
+		System.out.println(result.length);
 	}
 }
