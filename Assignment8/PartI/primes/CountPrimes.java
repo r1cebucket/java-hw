0656da5ba62f2fa6497687b89720bd79a5fb0fa9
Ricebucket <ricebucket.acc@gmail.com>
2023-07-29 01:38:19 +0800

    update

--- Assignment8/PartI/primes/CountPrimes.java
+++ Assignment8/PartI/primes/CountPrimes.java
@@ -1,26 +1,35 @@
 package primes;
 
+import java.util.HashMap;
+
 public class CountPrimes {
+	static HashMap<Long, Boolean> isPrime = new HashMap<>();
 
 	static boolean isPrime(long val) {
-        long upper_bound = (long) Math.floor(Math.sqrt(val));
-        
-        for (long i = 2; i <= upper_bound; i++) {
-            if (val % i == 0)
-                return false;
-        }
-        
-        return true;		
+		if (isPrime.containsKey(val)) {
+			return isPrime.get(val);
+		}
+
+		long upper_bound = (long) Math.floor(Math.sqrt(val));
+
+		for (long i = 2; i <= upper_bound; i++) {
+			if (val % i == 0) {
+				isPrime.put(val, false);
+				return false;
+			}
+		}
+		isPrime.put(val, true);
+		return true;
 	}
-	
+
 	static int numPrimes(long from, long to) {
 		int primeCount = 0;
-		for (long i = from ; i <= to; i++) {
+		for (long i = from; i <= to; i++) {
 			if (isPrime(i)) {
 				primeCount++;
 			}
 		}
 		return primeCount;
-		
+
 	}
 }
